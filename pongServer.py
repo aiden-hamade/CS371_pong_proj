4be6cf9cd4fc6f61a20a57975dff2fc1d1c56e77
BR0XICK <kessingerbrock@gmail.com>
2023-10-30 22:35:51 -0400

    Initial Server
    added setup for the two client connections

--- pongServer.py
+++ pongServer.py
@@ -10,7 +10,34 @@ import socket
 import threading
 
 # Use this file to write your server logic
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)      # Creating the server
+
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)    # Working on localhost need this
+
+server.bind(("localhost", 12321))
+server.listen(5)
+
 # You will need to support at least two clients
+
+connections = []
+
+clientSocket1, clientAddress1 = server.accept()
+clientSocket2, clientAddress2 = server.accept()
+
+connections.append(clientSocket1)
+connections.append(clientSocket2)
+
+# with keyword takes care of closing clients at the end of loop (need to double check the &)
+with clientSocket1 & clientSocket2:
+    while True:     # make into signal clients send whenever they quit the game
+        msg1 = clientSocket1.recv(1024).decode()          # Received message from client1
+        msg2 = clientSocket2.recv(1024).decode()          # Received message from client2
+        for conn in connections:
+            conn.send("some form of data".encode()) # sending info back to clients through connections
+
+# clientSocket.close()
+# server.close()
+
 # You will need to keep track of where on the screen (x,y coordinates) each paddle is, the score 
 # for each player and where the ball is, and relay that to each client
 # I suggest you use the sync variable in pongClient.py to determine how out of sync your two
